654542cc6c6f0d8bf7497e2656b6cd8d88c2c8fa
Theth1708 <123228758+Theth1708@users.noreply.github.com>
2023-09-23 17:51:41 -0300

    proyect

---
+++ SCRIPTS.js
@@ -0,0 +1,53 @@
+// Mostrar colores - hacer seleccion
+
+document.querySelector('.btn-selector').onclick = () =>{
+    document.querySelector('.selector-color').classList.toggle('active');
+  };
+  
+  let themeButtons = document.querySelectorAll('.color-buttons');
+  
+  themeButtons.forEach(color =>{
+  
+    color.addEventListener('click', () =>{
+      let dataColor = color.getAttribute('data-color');
+      document.querySelector(':root').style.setProperty('--primario', dataColor);
+    });
+  
+  });
+  
+  
+  // Formulario para envío
+  
+  const $form = document.querySelector('#form')
+  const $botonMailto = document.querySelector('#envio-mail')
+  
+  $form.addEventListener('submit', handleSubmit)
+  
+  function handleSubmit(event){
+    event.preventDefault()
+    const form = new FormData(this)
+    console.log(form.get('name'))
+    $botonMailto.setAttribute('href', `mailto:julianb@gmail.com?subject=${form.get('name')} [ ${form.get('email')} ]&body=${form.get('message')}`)
+    $botonMailto.click()
+  }
+  
+  // Traer Botón Ir Arriba:
+  let mybutton = document.getElementById("myBtn");
+  
+  // Mostrar Botón al hacer 20 px de scroll abajo
+  window.onscroll = function() {scrollFunction()};
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+  
+  // Ir Arriba al hacer click en el botón
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  } 
+  
